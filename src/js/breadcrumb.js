8794f87aac07c87776d8c5066fa42a4b500fb637
= <=>
2022-11-05 02:12:14 +0700

    update

--- src/js/breadcrumb.js
+++ src/js/breadcrumb.js
@@ -3,7 +3,6 @@ breadcrumb.innerHTML =`	<div class="bg-white	 my-4">
     <!-- Breadcrumb -->
     <nav class="d-flex ">
       <h6 class="mb-0">
-
         <a href="" class="text-reset">Home</a>
         <span><i class="fa-solid fa-terminal text-danger"></i></span>
         <a href="" class="text-reset">Analytics</a>
