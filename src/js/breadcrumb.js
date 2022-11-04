7fb0d245957770fb3d6766572caf323d49f1cb57
= <=>
2022-11-05 01:40:55 +0700

    update

--- src/js/breadcrumb.js
+++ src/js/breadcrumb.js
@@ -1,14 +1,14 @@
 const breadcrumb =document.querySelector("#breadcrumb");
 breadcrumb.innerHTML =`	<div class="bg-white	 my-4">
-    <h1 class="text-center">Dashboard</h1>
     <!-- Breadcrumb -->
-    <nav class="d-flex">
+    <nav class="d-flex ">
       <h6 class="mb-0">
+
         <a href="" class="text-reset">Home</a>
-        <span>/</span>
+        <span><i class="fa-solid fa-terminal text-danger"></i></span>
         <a href="" class="text-reset">Analytics</a>
-        <span>/</span>
-        <a href="" class="text-reset"><u>Dashboard</u></a>
+        <span><i class="fa-solid fa-terminal text-danger"></i></span>
+        <a href="" class="text-reset "><b> Dashboard</b></a>
       </h6>
     </nav>
     <!-- Breadcrumb -->
