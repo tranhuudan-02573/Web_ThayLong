80c33a26502d4a33b62475895b891bd91f645326
= <=>
2022-11-11 14:02:07 +0700

    admin

--- src/js/admin/header.js
+++ src/js/admin/header.js
@@ -1,6 +1,47 @@
 const header =document.querySelector("header#header");
-header.innerHTML =`  <!--Double navigation-->
-  <header>
+header.innerHTML =`
+
+<div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold text-uppercase">change pass</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body mx-3">
+        <div class="md-form mb-4">
+          <i class="fas fa-envelope prefix grey-text text-default"></i>
+          <input type="email" id="defaultForm-email" class="form-control validate">
+          <label data-error="wrong" data-success="right" for="defaultForm-email">Your email</label>
+        </div>
+
+
+
+        <div class="md-form mb-4">
+          <i class="fas fa-lock prefix grey-text text-default"></i>
+          <input type="password" id="defaultForm-pass" class="form-control validate">
+          <label data-error="wrong" data-success="right" for="defaultForm-pass">Your password</label>
+
+        </div>
+         <div class="md-form mb-4">
+          <i class="fas fa-lock prefix grey-text text-default"></i>
+          <input type="password" id="defaultForm-repass" class="form-control validate">
+          <label data-error="wrong" data-success="right" for="defaultForm-repass">nhap lai password</label>
+        </div>
+        </div>
+		<!-- Material checked -->
+<!-- Material checked -->
+      <div class="modal-footer d-flex justify-content-between">
+		<span>ban chua co tai khoan? <a href="html/signup.html" class="text-default">sign up</a></span>
+        <button class="btn btn-default ">change</button>
+      </div>
+    </div>
+  </div>
+</div>
+ <header>
     <!-- Sidebar navigation -->
     <div id="slide-out" class="side-nav fixed wide sn-bg-1">
       <ul class="custom-scrollbar">
@@ -14,62 +55,34 @@ header.innerHTML =`  <!--Double navigation-->
         <!-- Side navigation links -->
         <li>
           <ul class="collapsible collapsible-accordion">
-            <li><a class="collapsible-header waves-effect arrow-r active"><i class="sv-slim-icon fas fa-chevron-right"></i> Submit blog<i class="fas fa-angle-down rotate-icon"></i></a>
+            <li><a class="collapsible-header waves-effect arrow-r active"><i class="sv-slim-icon fas fa-chevron-right"></i> quan li<i class="fas fa-angle-down rotate-icon"></i></a>
               <div class="collapsible-body">
                 <ul>
-                  <li><a href="#" class="waves-effect active">
+                  <li><a href="/src/views/admin/manauser/index.html" class="waves-effect active">
                     <span class="sv-slim"> SL </span>
-                    <span class="sv-normal">Submit listing</span></a>
+                    <span class="sv-normal">quan li user</span></a>
                   </li>
-                  <li><a href="#" class="waves-effect">
+                  <li><a href="/src/views/admin/manaproduct/index.html" class="waves-effect">
                     <span class="sv-slim"> RF </span>
-                    <span class="sv-normal">Registration form</span></a>
+                    <span class="sv-normal">quan li san pham</span></a>
                   </li>
+                   <li><a href="/src/views/admin/manaorder/index.html" class="waves-effect">
+                    <span class="sv-slim"> RF </span>
+                    <span class="sv-normal">quan li don hang</span></a>
+                  </li>
+                   <li><a href="/src/views/admin/manasale/index.html" class="waves-effect">
+                    <span class="sv-slim"> RF </span>
+                    <span class="sv-normal">quan li uu dai</span></a>
+                  </li>
+                   <li><a href="/src/views/admin/manareview/index.html" class="waves-effect">
+                    <span class="sv-slim"> RF </span>
+                    <span class="sv-normal">quan li danh gia </span></a>
+                  </li>
+
                 </ul>
               </div>
             </li>
-            <li><a class="collapsible-header waves-effect arrow-r"><i class="sv-slim-icon far fa-hand-point-right"></i> Instruction<i class="fas fa-angle-down rotate-icon"></i></a>
-              <div class="collapsible-body">
-                <ul>
-                  <li><a href="#" class="waves-effect">
-                    <span class="sv-slim"> FB </span>
-                    <span class="sv-normal">For bloggers</span></a>
-                  </li>
-                  <li><a href="#" class="waves-effect">
-                    <span class="sv-slim"> FA </span>
-                    <span class="sv-normal">For authors</span></a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li><a class="collapsible-header waves-effect arrow-r"><i class="sv-slim-icon fas fa-eye"></i> About<i class="fas fa-angle-down rotate-icon"></i></a>
-              <div class="collapsible-body">
-                <ul>
-                  <li><a href="#" class="waves-effect">
-                    <span class="sv-slim"> I </span>
-                    <span class="sv-normal">Introduction</span></a>
-                  </li>
-                  <li><a href="#" class="waves-effect">
-                    <span class="sv-slim"> MM </span>
-                    <span class="sv-normal">Monthly meetings</span></a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li><a class="collapsible-header waves-effect arrow-r"><i class="sv-slim-icon far fa-envelope"></i> Contact me<i class="fas fa-angle-down rotate-icon"></i></a>
-              <div class="collapsible-body">
-                <ul>
-                  <li><a href="#" class="waves-effect">
-                    <span class="sv-slim"> F </span>
-                    <span class="sv-normal">FAQ</span></a>
-                  </li>
-                  <li><a href="#" class="waves-effect">
-                    <span class="sv-slim"> W </span>
-                    <span class="sv-normal">Write a message</span></a>
-                  </li>
-                </ul>
-              </div>
-            </li>
+
             <li><a id="toggle" class="waves-effect"><i class="sv-slim-icon fas fa-angle-double-left"></i>Minimize menu</a>
             </li>
           </ul>
@@ -91,25 +104,18 @@ header.innerHTML =`  <!--Double navigation-->
       </div>
       <ul class="nav navbar-nav nav-flex-icons ml-auto">
         <li class="nav-item">
-          <a class="nav-link"><i class="fas fa-envelope"></i> <span class="clearfix d-none d-sm-inline-block">Contact</span></a>
+          <a class="nav-link"><i class="fas fa-envelope"></i> <span class="clearfix d-none d-sm-inline-block">thoát</span></a>
+        </li>
+          <li class="nav-item">
+          <a class="nav-link" href="/src/views/admin/index.html"><i class="fas fa-envelope"></i> <span class="clearfix d-none d-sm-inline-block">trang chủ</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link"><i class="fas fa-comments"></i> <span class="clearfix d-none d-sm-inline-block">Support</span></a>
+          <a class="nav-link" data-target="#modal" data-toggle="modal"><i class="fas fa-comments"></i> <span class="clearfix d-none d-sm-inline-block">đổi mật khẩu</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link"><i class="fas fa-user"></i> <span class="clearfix d-none d-sm-inline-block">Account</span></a>
+          <a class="nav-link" href="/src/views/admin/profile.html"><i class="fas fa-user"></i> <span class="clearfix d-none d-sm-inline-block">thông tin cá nhân</span></a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            Dropdown
-          </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
+
       </ul>
     </nav>
     <!-- /.Navbar -->
